0f30e87cd3b273a18acec3930292ed131ef632b1
Oscar Tripicchio <oscar.tripicchio@denani>
2023-02-27 18:02:27 +0100

    nativebase & navigation

--- App.js
+++ App.js
@@ -11,9 +11,12 @@ import {
   extendTheme,
   VStack,
   Box,
+  Button,
 } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
 import { Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 // Define the config
 const config = {
@@ -21,48 +24,104 @@ const config = {
   initialColorMode: "dark",
 };
 
+const Stack = createNativeStackNavigator();
+
 // extend the theme
 export const theme = extendTheme({ config });
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Test" component={Test} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </NativeBaseProvider>
+  );
+}
+
+export function Home({ navigation }) {
+  return (
+    <Center
+      _dark={{ bg: "blueGray.900" }}
+      _light={{ bg: "blueGray.50" }}
+      px={4}
+      flex={1}
+    >
+      <VStack space={5} alignItems="center">
+        <NativeBaseIcon />
+        <Heading size="lg">Welcome to NativeBase</Heading>
+        <HStack space={2} alignItems="center">
+          <Text>Edit</Text>
+          <Box
+            _web={{
+              _text: {
+                fontFamily: "monospace",
+                fontSize: "sm",
+              },
+            }}
+            px={2}
+            py={1}
+            _dark={{ bg: "blueGray.800" }}
+            _light={{ bg: "blueGray.200" }}
+          >
+            App.js
+          </Box>
+          <Text>and save to reload.</Text>
+        </HStack>
+        <Link href="https://docs.nativebase.io" isExternal>
+          <Text color="primary.500" underline fontSize={"xl"}>
+            Learn NativeBase
+          </Text>
+        </Link>
+        <ToggleDarkMode />
+        <Button onPress={() => navigation.navigate("Test")}>
+          <Text>Go to Test</Text>
+        </Button>
+      </VStack>
+    </Center>
+  );
+}
+
+export function Test({ navigation }) {
+  return (
+    <Center
+      _dark={{ bg: "blueGray.900" }}
+      _light={{ bg: "blueGray.50" }}
+      px={4}
+      flex={1}
+    >
+      <VStack space={5} alignItems="center">
+        <NativeBaseIcon />
+        <Heading size="lg">Test Page</Heading>
+        <HStack space={2} alignItems="center">
+          <Text>Edit</Text>
+          <Box
+            _web={{
+              _text: {
+                fontFamily: "monospace",
+                fontSize: "sm",
+              },
+            }}
+            px={2}
+            py={1}
+            _dark={{ bg: "blueGray.800" }}
+            _light={{ bg: "blueGray.200" }}
+          >
+            App.js
+          </Box>
+          <Text>and save to reload.</Text>
+        </HStack>
+        <Link href="https://docs.nativebase.io" isExternal>
+          <Text color="primary.500" underline fontSize={"xl"}>
+            Learn NativeBase
+          </Text>
+        </Link>
+        <ToggleDarkMode />
+      </VStack>
+    </Center>
   );
 }
 
