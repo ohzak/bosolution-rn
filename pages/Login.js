3c27cff61e13ae79fc364093346bae4d18f9e49d
Oscar Tripicchio <oscar.tripicchio@denani.it>
2023-03-13 01:50:56 +0100

    login estetica e bg

--- pages/Login.js
+++ pages/Login.js
@@ -3,92 +3,131 @@ import {
   Box,
   Button,
   Center,
+  Container,
   FormControl,
   Heading,
   HStack,
   Input,
   Link,
   Text,
+  View,
   VStack,
 } from "native-base";
 import { LanguageContext } from "../translations/translationContext";
+import { Feather } from "@expo/vector-icons";
+import { ImageBackground } from "react-native-web";
 
 export function Login({ navigation }) {
   const { i18n } = useContext(LanguageContext);
 
   return (
-    <Center w="100%">
-      <Box safeArea p="2" py="8" w="90%" maxW="290">
-        <Heading
-          size="lg"
-          fontWeight="600"
-          color="coolGray.800"
-          _dark={{
-            color: "warmGray.50",
-          }}
+    <ImageBackground
+      source={require("../assets/background.png")}
+      style={{
+        backgroundColor: "#2e88a0",
+        flex: "1",
+        justifyContent: "center",
+      }}
+    >
+      <Center h="60%">
+        <Box
+          px={6}
+          w="90%"
+          maxW="400"
+          h="80%"
+          variant="main"
+          style={{ justifyContent: "center" }}
         >
-          {i18n.t("welcome")}
-        </Heading>
-        <Heading
-          mt="1"
-          _dark={{
-            color: "warmGray.200",
-          }}
-          color="coolGray.600"
-          fontWeight="medium"
-          size="xs"
-        >
-          {i18n.t("signin_continue")}
-        </Heading>
+          <Heading
+            size="lg"
+            fontWeight="600"
+            color="coolGray.800"
+            _dark={{
+              color: "warmGray.50",
+            }}
+          >
+            {i18n.t("welcome")}
+          </Heading>
+          <Heading
+            mt="1"
+            _dark={{
+              color: "warmGray.200",
+            }}
+            color="coolGray.600"
+            fontWeight="medium"
+            size="xs"
+          >
+            {i18n.t("signin_continue")}
+          </Heading>
 
-        <VStack space={3} mt="5">
-          <FormControl>
-            <FormControl.Label>{i18n.t("email")}</FormControl.Label>
-            <Input />
-          </FormControl>
-          <FormControl>
-            <FormControl.Label>{i18n.t("password")}</FormControl.Label>
-            <Input type="password" />
-            <Link
-              _text={{
-                fontSize: "xs",
-                fontWeight: "500",
-                color: "indigo.500",
-              }}
-              alignSelf="flex-end"
-              mt="1"
-            >
-              {i18n.t("password_forgot_question")}
-            </Link>
-          </FormControl>
-          <Button mt="2" colorScheme="indigo">
-            {i18n.t("sign_in")}
-          </Button>
-          <HStack mt="6" justifyContent="center">
-            <Text
-              fontSize="sm"
-              color="coolGray.600"
-              _dark={{
-                color: "warmGray.200",
-              }}
-            >
-              I'm a new user.{" "}
-            </Text>
-            <Link
-              _text={{
-                color: "indigo.500",
-                fontWeight: "medium",
-                fontSize: "sm",
-              }}
-              onPress={() => {
-                navigation.navigate("SignUp");
-              }}
-            >
-              Sign Up
-            </Link>
-          </HStack>
-        </VStack>
-      </Box>
-    </Center>
+          <VStack space={3} mt="5">
+            <FormControl>
+              <FormControl.Label>{i18n.t("email")}</FormControl.Label>
+              <Input
+                InputLeftElement={
+                  <Feather
+                    name="mail"
+                    size={16}
+                    color={"grey"}
+                    style={{ marginStart: "8px" }}
+                  />
+                }
+              />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>{i18n.t("password")}</FormControl.Label>
+              <Input
+                type="password"
+                InputLeftElement={
+                  <Feather
+                    name="lock"
+                    color={"grey"}
+                    size={16}
+                    style={{ marginStart: "8px" }}
+                  />
+                }
+              />
+              <Link
+                _text={{
+                  fontSize: "xs",
+                  fontWeight: "500",
+                  color: "primary.500",
+                }}
+                alignSelf="flex-end"
+                mt="1"
+              >
+                {i18n.t("password_forgot_question")}
+              </Link>
+            </FormControl>
+            <Button mt="2" colorScheme="primary">
+              {i18n.t("sign_in")}
+            </Button>
+            <HStack mt="6" justifyContent="center">
+              <Text
+                fontSize="sm"
+                color="coolGray.600"
+                _dark={{
+                  color: "warmGray.200",
+                }}
+              >
+                I'm a new user.{" "}
+              </Text>
+              <Link
+                _text={{
+                  color: "primary.500",
+                  fontWeight: "medium",
+                  fontSize: "sm",
+                }}
+                onPress={() => {
+                  navigation.navigate("SignUp");
+                }}
+              >
+                Sign Up
+              </Link>
+            </HStack>
+          </VStack>
+        </Box>
+      </Center>
+    </ImageBackground>
   );
 }
--- theme/themeContext.js
+++ theme/themeContext.js
@@ -12,6 +12,7 @@ import {
   Inter_800ExtraBold,
   Inter_900Black,
 } from "@expo-google-fonts/inter";
+import { ImageBackground } from "react-native";
 
 export const ThemeProvider = ({ children }) => {
   useFonts({
@@ -59,20 +60,71 @@ export const ThemeProvider = ({ children }) => {
     colors: {
       // Add new color
       primary: {
-        50: "#E3F2F9",
-        100: "#C5E4F3",
-        200: "#A2D4EC",
-        300: "#7AC1E4",
-        400: "#47A9DA",
-        500: "#0088CC",
-        600: "#007AB8",
-        700: "#006BA1",
-        800: "#005885",
-        900: "#003F5E",
+        50: "#E3F3FA",
+        100: "#B9E1F3",
+        200: "#8FD0EC",
+        300: "#65BEE5",
+        400: "#3CAEDF",
+        500: "#2394B2",
+        600: "#2e88a0",
+        700: "#1F5669",
+        800: "#173D4D",
+        900: "#0E2332",
+      },
+      warning: {
+        50: "#FFF5E5",
+        100: "#FFE1B8",
+        200: "#FFCD8A",
+        300: "#FFB65D",
+        400: "#FFA033",
+        500: "#FF8C0A",
+        600: "#E67800",
+        700: "#B36200",
+        800: "#804C00",
+        900: "#4F2D00",
+      },
+      danger: {
+        50: "#FEE3E3",
+        100: "#FCC5C5",
+        200: "#FAA7A7",
+        300: "#F88989",
+        400: "#F66B6B",
+        500: "#F44D4D",
+        600: "#E03F3F",
+        700: "#A62F2F",
+        800: "#802626",
+        900: "#5A1D1D",
+      },
+      success: {
+        50: "#E3F8F2",
+        100: "#B9EEDF",
+        200: "#8FE4CC",
+        300: "#65DAB9",
+        400: "#3CD0A6",
+        500: "#2EB08C",
+        600: "#279A7A",
+        700: "#1F6C4E",
+        800: "#174F38",
+        900: "#0E321F",
       },
       // Redefining only one shade, rest of the color will remain same.
       amber: {
         400: "#d97706",
+      },
+    },
+    components: {
+      Button: {
+        defaultProps: {
+          colorScheme: "primary",
+        },
+      },
+      Box: {
+        variants: {
+          main: {
+            rounded: "xl",
+            backgroundColor: "white",
+          },
+        },
       },
     },
     config: {
