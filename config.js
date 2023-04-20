3f6342f3007cfc44691f1eda818035f28ba946f5
Oscar Tripicchio <oscar.tripicchio@denani>
2023-04-20 18:33:25 +0200

    dotenv installed but not working

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
 export const HOST_API =
-  process.env.REACT_APP_HOST_API_KEY || "https://localhost:7116";
+  process.env.REACT_APP_HOST_API_KEY ??
+  "https://bosolution-api.azurewebsites.net";
 
 export const GOOGLE_PLACES_API_KEY = process.env.GOOGLE_PLACES_API_KEY;
